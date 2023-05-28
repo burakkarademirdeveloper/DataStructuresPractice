687e36c8cb9aea91fae8d4007fa551161d019290
Burak Karademir <94866565+burakkarademirdeveloper@users.noreply.github.com>
2023-05-28 17:20:07 +0300

    Added Sorting Algorithms Application

---
+++ SortingAlgorithms/SortDirection.cs
@@ -0,0 +1,7 @@
+﻿namespace SortingAlgorithms;
+
+public enum SortDirection
+{
+    Ascending = 0,
+    Descending = 1
+}
---
+++ SortingApp/Program.cs
@@ -0,0 +1,74 @@
+﻿
+
+using SortingAlgorithms;
+
+Console.WriteLine("Sıralamak istediğiniz diziyi kendiniz belirlemelisiniz.");
+Console.WriteLine("Diziniz kaç elemanlı olsun?");
+int arrayCount = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[arrayCount];
+Console.WriteLine("Dizinizin elemanlarını giriniz.");
+for (int i = 0; i < arrayCount; i++)
+{
+    Console.WriteLine((i+1)+". elemanı giriniz.");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Hangi algoritma ile sıralamak istiyorsunuz?");
+Console.WriteLine("1- Bubble Sort");
+Console.WriteLine("2- Insertion Sort");
+Console.WriteLine("3- Merge Sort");
+Console.WriteLine("4- Quick Sort");
+Console.WriteLine("5- Selection Sort");
+int algorithm = Convert.ToInt32(Console.ReadLine());
+switch (algorithm)
+{
+    case 1:
+        BubbleSort.Sort(array);
+        Console.WriteLine("Bubble sort ile sıralanmış diziniz:");
+        for (int i = 0; i < arrayCount; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+        break;
+    case 2:
+        InsertionSort.Sort(array);
+        Console.WriteLine("Insertion sort ile sıralanmış diziniz:");
+        for (int i = 0; i < arrayCount; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+        break;
+    case 3:
+        MergeSort.Sort(array);
+        Console.WriteLine("Merge sort ile sıralanmış diziniz:");
+        for (int i = 0; i < arrayCount; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+        break;
+    case 4:
+        Quicksort.Sort(array);
+        Console.WriteLine("Quick sort ile sıralanmış diziniz:");
+        for (int i = 0; i < arrayCount; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+        break;
+    case 5:
+        SelectionSort.Sort(array);
+        Console.WriteLine("Selection sort ile sıralanmış diziniz:");
+        for (int i = 0; i < arrayCount; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+        break;
+    default:
+        Console.WriteLine("Hatalı seçim yaptınız.");
+        break;
+}
+
+
